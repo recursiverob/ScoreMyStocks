d15e63f98750f72db10085c13015698dd48a00a5
recursiverob <138645641+recursiverob@users.noreply.github.com>
2025-01-22 01:17:02 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Include all JS/TSX files in the src folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
